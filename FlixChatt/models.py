f858a11c2283c048378ea37fbe805010c0abd171
Dan-njuguna <njugush020@gmail.com>
2024-07-26 21:13:59 +0300

    Changed: FlixChatt/models.py

--- FlixChatt/models.py
+++ FlixChatt/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.db import DatabaseError
+import time
 
-# Create your models here.
+class User(models.Model):
+    def __init__(self):
+        self.firstName = models.CharField(max_length = 50)
+        self.lastName = models.CharField(max_length = 50)
+        self.username = models.CharField(max_length = 50)
+        self.email = models.EmailField(max_length = 50)
+        self.profileImage = models.ImageField(upload_to = "profile_images")
+        self.userId = models.IntegerField(max_length = 8, default = 0)
+
+    def __str__(self):
+        return self.username
+    
+class Message(models.Model):
+    def __init__(self):
+        self.message = models.CharField(max_length = 1024)
+        self.imageFile = models.ImageField(upload_to = "shared_images")
+        self.timeStamp = models.DateField(max_length = 8)
+
+    def __str__(self):
+        return self.message
+    
+    
